e2ddd69b698d4e4181e11a4de697b1217e7c55fc
Booi <56810550+booimangang@users.noreply.github.com>
2022-12-31 12:16:01 +0530

    Add check toggle feature to todo entries

--- src/todoEntryCreate.js
+++ src/todoEntryCreate.js
@@ -9,7 +9,7 @@ import "./todoEntryCreate.css";
         titleEntryDiv.prepend(el)
     }
 
-    const createEntryTodo = function ({ id, title, group, check, dueDate }) {
+    const createEntryTodo = function (todo) {
 
         let titleEntry = DOM.createEl('div')
         let titleGroupDiv = DOM.createEl('div')
@@ -28,59 +28,28 @@ import "./todoEntryCreate.css";
         titleEl.classList.add('title--entry')
         inGroup.classList.add('group--entry')
         isCheck.classList.add('check--entry')
-        if (check) isCheck.classList.add('checked')
+        if (todo.check) isCheck.classList.add('checked')
         dueDateEL.classList.add('due-date--entry')
         delBtn.classList.add('del-btn--entry')
 
         const setElementAtt = function () {
             titleEntry.setAttribute('data-entry', '')
-            titleEntry.setAttribute('data-entry-id', id)
-            titleEl.setAttribute('data-title-id', id)
-            inGroup.setAttribute('id', `group-${id}`)
-            isCheck.setAttribute('id', `check-${id}`)
-            delBtn.setAttribute('data-del-id', id)
+            titleEntry.setAttribute('data-entry-id', todo.id)
+            titleEl.setAttribute('data-title-id', todo.id)
+            inGroup.setAttribute('id', `group-${todo.id}`)
+            isCheck.setAttribute('id', `check-${todo.id}`)
+            delBtn.setAttribute('data-del-id', todo.id)
         }
 
         // set element's VALUE & TEXT
 
         const setElementsValueNtext = function () {
             isCheck.innerText = '';
-            inGroup.innerText = `> ${group}`;
-            titleEl.innerText = title;
-            dueDateEL.innerText = dueDate;
+            inGroup.innerText = `> ${todo.group}`;
+            titleEl.innerText = todo.title;
+            dueDateEL.innerText = todo.dueDate;
             delBtn.innerText = 'DD'
         }
-
-        // bind EVENTS
-
-        let entryViewContainer = DOM.find('.entry-backdrop--view')
-
-        let delBtnEventHandler = function (e) {
-            let id = e.target.dataset.delId;
-            PS.trigger('removeEntry', id)
-            PS.trigger('delTodo', id)
-        }
-
-        let displayEntryViewModal = function (id) {
-            if (!id) return
-            let todo = PS.trigger("readTodo", id)
-            PS.trigger("todoEntryView", todo)
-            entryViewContainer.classList.remove('hidden')
-        }
-
-        let titleClickEventHandler = function (e) {
-            let id = e.target.dataset.titleId;
-            displayEntryViewModal(id)
-        }
-
-        let entryClickEventHandler = function (e) {
-            let id = e.target.dataset.entryId;
-            displayEntryViewModal(id)
-        }
-
-        delBtn.addEventListener('click', delBtnEventHandler)
-        titleEl.addEventListener('click', titleClickEventHandler)
-        titleEntry.addEventListener('click', entryClickEventHandler)
 
         // RENDER
 
@@ -90,6 +59,44 @@ import "./todoEntryCreate.css";
             titleEntry.append(isCheck, titleGroupDiv, header)
             addEntry(titleEntry)
         }
+
+        // bind EVENTS
+
+        let entryViewContainer = DOM.find('.entry-backdrop--view')
+
+        const delBtnEventHandler = function (e) {
+            let id = e.target.dataset.delId;
+            PS.trigger('removeEntry', id)
+            PS.trigger('delTodo', id)
+        }
+
+        const displayEntryViewModal = function (id) {
+            if (!id) return
+            let todo = PS.trigger("readTodo", id)
+            PS.trigger("todoEntryView", todo)
+            entryViewContainer.classList.remove('hidden')
+        }
+
+        const titleClickEventHandler = function (e) {
+            let id = e.target.dataset.titleId;
+            displayEntryViewModal(id)
+        }
+
+        const entryClickEventHandler = function (e) {
+            let id = e.target.dataset.entryId;
+            displayEntryViewModal(id)
+        }
+
+        const toggleCheckBtnHandler = function (e) {
+            let updatedTodo = { ...todo, check: !todo.check }
+            PS.trigger('updateTodo', updatedTodo)
+            todo.check = !todo.check
+        }
+
+        delBtn.addEventListener('click', delBtnEventHandler)
+        titleEl.addEventListener('click', titleClickEventHandler)
+        titleEntry.addEventListener('click', entryClickEventHandler)
+        isCheck.addEventListener('click', toggleCheckBtnHandler)
 
         // CREATE func
 
